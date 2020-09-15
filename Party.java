d83f0bf561211fdd86819df791950aed0b7179c7
Victoria <ratom_v@mail.ru>
2020-09-15 16:04:42 +0300

    Created new file Party.java

---
+++ Party.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+class Party{
+    public void buildInvite(){
+        Frame f = new Frame();
+        Label l = new Label("Вечеринка у Тима");
+        Button b = new Button("Сбросить");
+        Panel p = new Panel();
+        p.add(l);
+    } //Еще код ...
+}
